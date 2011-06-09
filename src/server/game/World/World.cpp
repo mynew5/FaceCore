e559b1784441cdc0a617eb20b8510b2e98bb9b8a
unknown <Junior@.(none)>
2011-06-09 09:49:16 -0300

    INSERT > REPLACE

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2106,7 +2106,7 @@ void World::SendGlobalGMMessage(WorldPacket *packet, WorldSession *self, uint32 
             itr->second->GetPlayer() &&
             itr->second->GetPlayer()->IsInWorld() &&
             itr->second != self &&
-            itr->second->GetSecurity() > SEC_PLAYER &&
+            itr->second->GetSecurity() > SEC_MODERATOR &&
             (team == 0 || itr->second->GetPlayer()->GetTeam() == team))
         {
             itr->second->SendPacket(packet);
