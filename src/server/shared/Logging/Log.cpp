ec9eb5fa77f8191470557fb9a8578a05d9a13f47
unknown <Junior@.(none)>
2011-06-10 20:23:35 -0300

    FIX

--- src/server/shared/Logging/Log.cpp
+++ src/server/shared/Logging/Log.cpp
@@ -468,7 +468,7 @@ void Log::outCrash(const char * err, ...)
 
 void Log::outError(const char * err, ...)
 {
-    // return; // disable this function
+    return; // disable this function
     if (!err)
         return;
 
