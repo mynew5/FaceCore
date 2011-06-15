4d0921e09c9b236bd704222fc1a684d759c129ff
unknown <Junior@.(none)>
2011-06-15 18:02:39 -0300

    [Core/Misc] Fix a server crash and small issue related to casting Blink inside transports

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2167,7 +2167,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
     if (duel && GetMapId() != mapid && GetMap()->GetGameObject(GetUInt64Value(PLAYER_DUEL_ARBITER)))
         DuelComplete(DUEL_FLED);
 
-    if ((GetMapId() == mapid && !m_transport) || (GetTransport() && GetMapId() == 628))
+    if (GetMapId() == mapid) // ((GetMapId() == mapid && !m_transport) || (GetTransport() && GetMapId() == 628))
     {
         m_anti_JumpBaseZ = 0;
         //lets reset far teleport flag if it wasn't reset during chained teleports
