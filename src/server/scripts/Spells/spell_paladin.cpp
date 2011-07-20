LORDofDOOM/MMOCore

commit 259001a5147852b8711e5bb61383c779ae71f7ab

Spells/Paladin: Add cooldown for Sacred Shield after remove effect as expected

src/server/scripts/Spells/spell_paladin.cpp
@@ -29,6 +29,8 @@ enum PaladinSpells
     PALADIN_SPELL_DIVINE_PLEA                    = 54428,
     PALADIN_SPELL_BLESSING_OF_SANCTUARY_BUFF     = 67480,
 
+    PALADIN_SPELL_SACRED_SHIELD_EFFECT           = 58597,
+
     PALADIN_SPELL_BLESSING_OF_SANCTUARY_HELPER   = 20912,
 
     PALADIN_SPELL_HOLY_SHOCK_R1                  = 20473,
@@ -301,6 +303,40 @@ public:
     }
 };
 
+// 58597 Sacred shield add cooldown
+class spell_pal_sacred_shield : public SpellScriptLoader
+{
+public:
+    spell_pal_sacred_shield() : SpellScriptLoader("spell_pal_sacred_shield") { }
+
+    class spell_pal_sacred_shield_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_pal_sacred_shield_AuraScript)
+        bool Validate(SpellEntry const* /*entry*/)
+        {
+            if (!sSpellStore.LookupEntry(PALADIN_SPELL_SACRED_SHIELD_EFFECT))
+                return false;
+            return true;
+        }
+
+        void HandleEffectRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        {
+            if (Player* caster = GetCaster()->ToPlayer())
+                caster->AddSpellCooldown(PALADIN_SPELL_SACRED_SHIELD_EFFECT, 0, time(NULL) + 6);
+        }
+
+        void Register()
+        {
+            AfterEffectRemove += AuraEffectRemoveFn(spell_pal_sacred_shield_AuraScript::HandleEffectRemove, EFFECT_1, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL_OR_REAPPLY_MASK);
+        }
+    };
+
+    AuraScript *GetAuraScript() const
+    {
+        return new spell_pal_sacred_shield_AuraScript();
+    }
+};
+
 class spell_pal_judgement_of_command : public SpellScriptLoader
 {
 public:
@@ -334,6 +370,7 @@ void AddSC_paladin_spell_scripts()
     new spell_pal_ardent_defender();
     new spell_pal_blessing_of_faith();
     new spell_pal_blessing_of_sanctuary();
+    new spell_pal_sacred_shield();
     new spell_pal_guarded_by_the_light();
     new spell_pal_holy_shock();
     new spell_pal_judgement_of_command();

sql/updates/fc_updates/facecore_updates_world.sql
@@ -834,3 +834,8 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment
 DELETE FROM `spell_script_names` WHERE `spell_id`=45390;
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (45390, 'spell_gen_ribbon_pole_dancer_check');
+
+-- Add cooldown for Sacred Shield after remove effect as expected
+DELETE FROM `spell_script_names` WHERE `spell_id` = 58597;
+INSERT INTO `spell_script_names` VALUES
+(58597, 'spell_pal_sacred_shield');

mynew5/FaceCore

commit 46d76d1b1c5026c512821e4d95d2d3b0d587fbcc

[Crash/Fix] Fix crash introduced with revision 259001a5147852b8711e5bb61383c779ae71f7ab

src/server/scripts/Spells/spell_paladin.cpp
@@ -321,8 +321,9 @@ public:
 
         void HandleEffectRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
         {
-            if (Player* caster = GetCaster()->ToPlayer())
-                caster->AddSpellCooldown(PALADIN_SPELL_SACRED_SHIELD_EFFECT, 0, time(NULL) + 6);
+            if (Unit* caster = GetCaster())
+                if (Player* player = caster->ToPlayer())
+                    player->AddSpellCooldown(PALADIN_SPELL_SACRED_SHIELD_EFFECT, 0, time(NULL) + 6);
         }
 
         void Register()