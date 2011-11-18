da2c9daa636288a10d3c289c9b00f2273ae7b8cd
Kandera <kanderacutie@hotmail.com>
2011-11-18 14:57:30 -0500

    Core/Mechanic: Resurrection (fixed the issue with players ressing in place then being ported to the person who revived them.)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -5098,8 +5098,6 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
     if (GetSession()->IsARecruiter() || (GetSession()->GetRecruiterId() != 0))
         SetFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_REFER_A_FRIEND);
 
-    setDeathState(ALIVE);
-
     SetMovement(MOVE_LAND_WALK);
     SetMovement(MOVE_UNROOT);
 
@@ -5128,6 +5126,8 @@ void Player::ResurrectPlayer(float restore_percent, bool applySickness)
 
     // update visibility
     UpdateObjectVisibility();
+
+    setDeathState(ALIVE);
 
     if (!applySickness)
         return;
