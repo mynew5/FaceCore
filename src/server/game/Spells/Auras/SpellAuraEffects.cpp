ffc68008770d3271283ab4cf19e7099c5c51ec6b
unknown <Jr@Jr-PC.(none)>
2012-06-09 04:01:22 -0300

    Revert "wintergrasp"
    This reverts commit 56cfb58e05c747d2a48878e25cf4a86279ea01e5.

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -5138,15 +5138,9 @@ void AuraEffect::HandleAuraDummy(AuraApplication const* aurApp, uint8 mode, bool
                             target->CastSpell((Unit*)NULL, GetAmount(), true, NULL, this);
                             break;
                         case 58600: // Restricted Flight Area
-                            if (aurApp->GetRemoveMode() == AURA_REMOVE_BY_EXPIRE)
-                                target->CastSpell(target, 58601, true);
-                            break;
                         case 58730: // Restricted Flight Area
                             if (aurApp->GetRemoveMode() == AURA_REMOVE_BY_EXPIRE)
-                            {
                                 target->CastSpell(target, 58601, true);
-                                target->CastSpell(target, 45472, true);
-                            }
                             break;
                     }
                     break;
