4ec7a0e92e8968c7812edcc8a12de9ee269f9d79
Havenard <havenard@hotmail.com>
2013-02-25 14:38:04 -0300

    Ninja Inquisitor Adjusts

--- src/server/game/NinjaInquisitor/NinjaInquisitor.sql
+++ src/server/game/NinjaInquisitor/NinjaInquisitor.sql
@@ -7,7 +7,15 @@ CREATE TABLE `ninja_inquisitor_logs` (
     `time` INT UNSIGNED NOT NULL,
     `instance` INT UNSIGNED NOT NULL,
     `guid` INT UNSIGNED NOT NULL,
-    `event` TEXT NOT NULL,
-	FOREIGN KEY (`instance`) REFERENCES `instance` (`id`) ON DELETE CASCADE
-) ENGINE=MyISAM;
+    `event` TEXT NOT NULL
+) ENGINE=InnoDB;
 
+DROP TRIGGER IF EXISTS `ninja_inquisitor_delete`;
+
+DELIMITER $$
+CREATE TRIGGER `ninja_inquisitor_delete` BEFORE DELETE ON `instance` FOR EACH ROW
+BEGIN
+    DELETE FROM `ninja_inquisitor_logs` WHERE `instance` = `old`.`id`;
+END$$
+
+DELIMITER ;
