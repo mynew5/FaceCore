b549ec85829ae24cc077b89d85ab30cb7538f1d3
Havenard <havenard@hotmail.com>
2013-03-07 01:00:59 -0300

    Fix missing line

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -696,6 +696,7 @@ void AddSpellScripts()
 
 void AddCommandScripts()
 {
+    AddSC_vistawow_commandscript();
     AddSC_anticheat_commandscript();
     AddSC_account_commandscript();
     AddSC_achievement_commandscript();
