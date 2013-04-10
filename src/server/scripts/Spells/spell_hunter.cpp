a318e33fd3bdbc7d153e7ab87449ddb4876a374c
amsjunior1 <amsjunior1@hotmail.com>
2013-04-09 23:11:08 -0300

    Fix hammer of the righteous double pct damage mods.

--- src/server/scripts/Spells/spell_hunter.cpp
+++ src/server/scripts/Spells/spell_hunter.cpp
@@ -692,7 +692,7 @@ class spell_hun_sniper_training : public SpellScriptLoader
                         {
                             SpellInfo const* triggeredSpellInfo = sSpellMgr->GetSpellInfo(spellId);
                             Unit* triggerCaster = triggeredSpellInfo->NeedsToBeTriggeredByCaster() ? caster : target;
-                            triggerCaster->CastSpell(target, triggeredSpellInfo, true, 0, aurEff);
+                            triggerCaster->CastSpell(target, triggeredSpellInfo, true, 0, aurEff, aurEff->GetCasterGUID());
                         }
                 }
             }
