39c01fb788dcee038f7ea749f4f705e240a54ec2
Havenard <havenard@hotmail.com>
2013-05-14 12:55:56 -0300

    Fix SQL

--- sql/custom/damage_counter.sql
+++ sql/custom/damage_counter.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS `dps_counters`;
 
 CREATE TABLE `dps_counters` (
 	`entry` INT UNSIGNED NOT NULL,
+	`mode` INT UNSIGNED NOT NULL,
 	`guid` INT UNSIGNED NOT NULL,
 	`dps` FLOAT NOT NULL,
-	PRIMARY KEY (`entry`, `guid`)
+	PRIMARY KEY (`entry`, `mode`, `guid`)
 );
