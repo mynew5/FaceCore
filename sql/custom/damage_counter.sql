c8b8fe2dae1619f0a6df413732542e08431e1441
Havenard <havenard@hotmail.com>
2013-05-14 10:21:17 -0300

    DamageCounter (untested)

---
+++ sql/custom/damage_counter.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `dps_counters`;
+
+CREATE TABLE `dps_counters` (
+	`entry` INT UNSIGNED NOT NULL,
+	`guid` INT UNSIGNED NOT NULL,
+	`dps` FLOAT NOT NULL,
+	PRIMARY KEY (`entry`, `guid`)
+);
