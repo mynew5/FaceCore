6575bb6baf5feb37ff20cfb2ea374b65cecd073b
Havenard <havenard@hotmail.com>
2013-06-13 22:52:13 -0300

    Temporarily disable FleeingMovementGenerator until #7704 is fixed

--- src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.cpp
@@ -32,6 +32,8 @@
 template<class T>
 void FleeingMovementGenerator<T>::_setTargetLocation(T* owner)
 {
+    return; // Temporarily disable FleeingMovementGenerator until #7704 is fixed
+
     if (!owner)
         return;
 
