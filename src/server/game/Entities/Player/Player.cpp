9ce8bd2ccc173d33afeb241b7cc1b8f42606fd3b
amsjunior1 <amsjunior1@hotmail.com>
2013-08-24 21:58:44 -0300

    Warriors CAN use some spells immediately

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -12422,8 +12422,7 @@ Item* Player::EquipItem(uint16 pos, Item* pItem, bool update)
 
                 if (!spellProto)
                     TC_LOG_ERROR(LOG_FILTER_PLAYER, "Weapon switch cooldown spell %u couldn't be found in Spell.dbc", cooldownSpell);
-                else
-
+                else if (getClass() != CLASS_WARRIOR) // FIXME: Warriors CAN use some spells immediately after swapping weapons, but probably not any spell
                 {
                     m_weaponChangeTimer = spellProto->StartRecoveryTime;
 
